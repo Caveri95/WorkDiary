cde9d97e3ae3c25e93377895e4ff34d6e677f785
helloworld <ura22051995@gmail.com>
2023-02-12 20:39:20 +0300

    Добавлена задача на функциональное программирование

--- src/FunctionalTask/Main.java
+++ src/FunctionalTask/Main.java
@@ -1,96 +1,27 @@
 package FunctionalTask;
 
+
 import java.util.*;
 import java.util.function.Function;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in).useDelimiter("\\n");
+        String text = scanner.next();
 
+        int wordsCount = Arrays.stream(text.replaceAll("\\p{Punct}", "").split(" "))
+                .collect(Collectors.toList())
+                .size();
+        System.out.println("Количество слов в тексте " + wordsCount);
 
-//        System.out.println(Stream.of(1, 2, 3, 4, 9).allMatch(x -> x <= 7));
-//        Set<Integer> set = Stream.of(99, 2, 3).collect(Collectors.toSet());
-//        System.out.println(set);
-//        Long count = Stream.of("1", "2", "3", "4").count();
-//        System.out.println(count);
-
-        /*String text = "yourapp the quick brown fox jumps over the lazy dog";
-
-
-        Pattern pattern = Pattern.compile("\\s");
-        String[] strings = pattern.split(text);
-        Arrays.stream(strings).forEach(System.out::println);
-        System.out.println("Количество слов в тексте - " + strings.length);
-        System.out.println(Arrays.stream(strings).collect(Collectors.toList()).get(1));*/
-
-        /*Pattern p = Pattern.compile("a*b");// скомпилировали регулярное выражение в представление
-        Matcher m = p.matcher("aaaaab");
-        System.out.println(m);*/
-
-        String textInput = "the? quick, brown/ fox. themps over the  the fox the lazy dog";
-        String text = textInput.replaceAll("\\p{Punct}", "");
-
-
-        Pattern pattern = Pattern.compile("\\s");
-        String[] strings = pattern.split(text);
-        String word = " " + Arrays.stream(strings).collect(Collectors.toList()).get(0) + " ";
-        System.out.println("ищем" + word);
-
-
-
-
-        Pattern pattern1 = Pattern.compile(word);
-        Matcher matcher = pattern1.matcher(text);
-        while (matcher.find()) {
-            int start=matcher.start();
-            int end=matcher.end();
-            System.out.println("Найдено совпадение " + text.substring(start,end) + " с "+ start + " по " + (end-1) + " позицию");
-        }
-
-
-
-        /*ArrayList<String> words = new ArrayList<>();
-        for (String s : sentence.split(" ")) {
-            words.add(s.replaceAll("[^a-zA-Zа-яёА-ЯЁ]", "").toLowerCase());
-        }
-
-        words.stream().collect(Collectors.toMap(Function.identity(), v -> 1L, Long::sum));
-
-
-        HashSet<String> distinct = new HashSet<>();
-        HashSet<String> repetitive = new HashSet<>();
-        for (String i: strings) {
-            if (distinct.contains(i)) {
-                repetitive.add(i);
-            } else {
-                distinct.add(i);
-            }
-        }*/
-        /*repetitive.stream().forEach(System.out::println);
-        System.out.println(repetitive.size());
-        System.out.println("______________");
-        distinct.stream().forEach(System.out::println);
-        //System.out.println(repetitive.size());*/
-
-
-        /*System.out.println(matcher.replaceFirst("Ира"));
-        System.out.println(matcher.replaceAll("Ольга"));*/
-        //System.out.println(text);
-
-        long a = 10;
-        long b = 7;
-
-
-        System.out.println(a / b);
-
-
-
-
-
+        Arrays.stream(text.replaceAll("\\p{Punct}", "").split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder()).thenComparing(Map.Entry.<String, Long>comparingByKey()))
+                .limit(10)
+                .forEach(e -> System.out.println(e.getKey() + " повторений в тексте " + e.getValue()));
     }
 }
